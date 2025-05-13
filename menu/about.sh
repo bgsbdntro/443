4551c563af49815dff261785ef326a0023fa5202
bgsbdntro <bagusbb694@gmail.com>
2025-05-13 14:35:21 +0700

    first commit

---
+++ menu/about.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+
+clear
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " \e[0;100;33m        • AutoScript by Bagus Toro •            \e[0m"
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "                 ICN NETWORK ID                   " 
+echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""                                                                                        "
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
+
+
+
